9882293f6489b2ba953b1b65897a712883de4fe1
Arseny Khoroshilov <horoshilov.aa@phystech.edu>
2020-06-27 18:22:43 +0300

    initial commit

---
+++ bot.go
@@ -0,0 +1,118 @@
+package main
+
+import (
+	"fmt"
+	"github.com/go-telegram-bot-api/telegram-bot-api"
+	"log"
+	"net/url"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func keyboardFromSearchResults(results []string) (tgbotapi.InlineKeyboardMarkup, bool) {
+	var keyboard tgbotapi.InlineKeyboardMarkup
+	tooLong := len(results) > 10
+	if tooLong {
+		results = results[:11]
+	}
+	for _, name := range results {
+		button := tgbotapi.NewInlineKeyboardButtonData(name, name)
+		keyboard.InlineKeyboard = append(keyboard.InlineKeyboard, []tgbotapi.InlineKeyboardButton{button})
+	}
+	return keyboard, tooLong
+}
+
+func sendPhoto(bot *tgbotapi.BotAPI, chatID int64, link, caption string) (tgbotapi.APIResponse, error) {
+	params := url.Values{
+		"chat_id":[]string{strconv.FormatInt(chatID, 10)},
+		"photo":[]string{link},
+		"caption":[]string{caption},
+		"parse_mode":[]string{"MarkdownV2"},
+	}
+	return bot.MakeRequest("sendPhoto", params)
+}
+
+var (
+	statNames = [5]string{
+		"Знания",
+		"Умение преподавать",
+		"В общении",
+		"«Халявность»",
+		"Общая оценка",
+	}
+)
+
+var TOKEN = os.Getenv("WIKIMIPT_TGTOKEN")
+
+func main() {
+	bot, err := tgbotapi.NewBotAPI(TOKEN)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates, err := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.CallbackQuery != nil {
+			profile, err := getProfile(update.CallbackQuery.Data)
+			if err != nil {
+				log.Panic(err)
+			} else {
+				text := "*"+profile.name+"*\n\n"+profile.desc
+				for index, stat := range profile.stats {
+					text += "_*" + statNames[index] + "*_:\n"
+					if stat.votes != 0 {
+						for i := 0; i < 5; i++ {
+							if i < int(stat.value) {
+								text += "\U00002B50"
+							} else {
+								text += "\U0001F311"
+							}
+						}
+						text += escapeMarkdownV2(fmt.Sprintf(" %4.2f (%d голосов)\n", stat.value, stat.votes))
+					} else {
+						for i := 0; i < 5; i++ {text += "\U0001F31A"}
+						text += escapeMarkdownV2(" (нет голосов)\n")
+					}
+				}
+				text += fmt.Sprintf("[_Страница на wikimipt_](%s)", strings.ReplaceAll(pageUrl(profile.name), ")", "\\)"))
+				_, err = sendPhoto(bot, update.CallbackQuery.Message.Chat.ID, profile.photo, text)
+			}
+		}
+		if update.Message != nil {
+			var msg tgbotapi.MessageConfig
+
+			results, err := search(update.Message.Text)
+			if err == badCharError {
+				msg = tgbotapi.NewMessage(update.Message.Chat.ID, "Неподдерживаемые символы в запросе")
+			}else if err != nil {
+				log.Panic(err)
+			} else {
+				if len(results) != 0 {
+					msg = tgbotapi.NewMessage(update.Message.Chat.ID, "Результаты поиска:")
+					msg.ParseMode = "MarkdownV2"
+					msg.ReplyToMessageID = update.Message.MessageID
+					var tooLong bool
+					msg.ReplyMarkup, tooLong = keyboardFromSearchResults(results)
+					if tooLong {
+						msg.Text += "\n_\\(первые 10 результатов\\)_"
+					}
+				} else {
+					msg = tgbotapi.NewMessage(update.Message.Chat.ID, "Ничего не найдено")
+					msg.ReplyToMessageID = update.Message.MessageID
+				}
+			}
+			if _, err := bot.Send(msg); err != nil {
+				log.Panic(err)
+			}
+		}
+	}
+}
---
+++ wiki.go
@@ -0,0 +1,173 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"golang.org/x/net/html"
+	"log"
+	"regexp"
+	"strconv"
+	"strings"
+
+	"github.com/anaskhan96/soup"
+)
+
+func pageUrl(name string) string {
+	return "http://wikimipt.org/wiki/" + name
+}
+
+var badCharError = errors.New("bad symbols in query")
+const searchUrl = "http://wikimipt.org/index.php?title=Категория:Преподаватели_по_алфавиту&from="
+var nonCyrillicRE = regexp.MustCompile("[^а-я ]")
+func search(query string) ([]string, error){
+	query = strings.ReplaceAll(strings.ToLower(query), "ё", "е")
+	if nonCyrillicRE.FindStringIndex(query) != nil {
+		return []string{}, badCharError
+	}
+	queryRune := []rune(query)
+
+	resp, err := soup.Get(searchUrl+string(queryRune[0]))
+	if err != nil {
+		log.Fatal(err)
+		return []string{}, err
+	}
+	doc := soup.HTMLParse(resp)
+	as := doc.FindStrict("div", "class", "mw-category-group").FindAll("a")
+
+	var results []string
+	for _, a := range as {
+		name := a.Attrs()["title"]
+		if strings.HasPrefix(strings.ToLower(name), query) {
+			results = append(results, name)
+		}
+	}
+
+	return results, nil
+}
+
+type mark struct {
+	value float64
+	votes int64
+}
+type teacherProfile struct {
+	name string
+	photo string
+	stats [5]mark
+	desc string
+}
+
+var (
+	tagHeads = map[string]string{
+		"i":"_",
+		"b":"*",
+		"p":"",
+		"ul":"",
+		"li":" • ",
+		"br":"\n",
+	}
+	tagTails = map[string]string{
+		"i":"_",
+		"b":"*",
+		"p":"\n",
+		"ul":"",
+		"li":"\n",
+		"br":"",
+	}
+)
+var (
+	mustEscapeRE = regexp.MustCompile("["+regexp.QuoteMeta("_*[]()~`>#+=|{}.!-")+"]")
+	nonWhitespaceRE = regexp.MustCompile("\\S")
+)
+func escapeMarkdownV2(s string) string {
+	return mustEscapeRE.ReplaceAllStringFunc(s, func(ss string) string {return "\\"+ss})
+}
+func parseTag(tag soup.Root) (string, error) {
+	if tag.Pointer.Type == html.TextNode {
+		if nonWhitespaceRE.FindStringIndex(tag.NodeValue) == nil {
+			return "", nil
+		}
+		return escapeMarkdownV2(tag.NodeValue), nil
+	}
+	if tag.Pointer.Type == html.CommentNode {
+		return "", nil
+	}
+
+	result := ""
+	if tag.NodeValue == "a" {
+		url := strings.ReplaceAll(tag.Attrs()["href"], ")", "\\)")
+		if url[0:1] == "/" {
+			url = "wikimipt.org" + url
+		}
+		result += "["
+		for _, child := range tag.Children() {
+			subres, err := parseTag(child)
+			if err != nil {
+				return "", err
+			}
+			result += subres
+		}
+		result += fmt.Sprintf("](%s)", url)
+	} else if head, ok := tagHeads[tag.NodeValue]; ok {
+		result += head
+		for _, child := range tag.Children() {
+			subres, err := parseTag(child)
+			if err != nil {
+				return "", err
+			}
+			result += subres
+		}
+		result += tagTails[tag.NodeValue]
+	} else {
+		return "", errors.New("not supported tag: "+tag.NodeValue)
+	}
+	return result, nil
+}
+
+func getProfile (name string) (teacherProfile, error) {
+	link := pageUrl(name)
+	resp, err := soup.Get(link)
+	if err != nil {
+		return teacherProfile{}, err
+	}
+
+	var result teacherProfile
+	result.name = name
+
+	doc := soup.HTMLParse(resp)
+	table := doc.FindStrict("table", "class", "wikitable card")
+	if img := table.FindAll("tr")[1].Find("img"); img.Pointer != nil {
+		result.photo = "wikimipt.org" + img.Attrs()["src"]
+	}
+	statsRows := table.Find("table").FindAll("tr")
+	for index, row := range statsRows {
+		var (
+			value float64
+			votes int64
+		)
+		text := row.FindStrict("span", "class", "starrating-avg").FullText()
+		if text == "( нет голосов )" {
+			value = 0.
+			votes = 0
+		} else {
+			splitText := strings.Split(text, " ")
+			var err error
+			if value, err = strconv.ParseFloat(splitText[0], 32); err != nil {
+				return teacherProfile{}, err
+			}
+			if votes, err = strconv.ParseInt(splitText[2], 10, 0); err != nil {
+				return teacherProfile{}, err
+			}
+		}
+		result.stats[index] = mark{value, votes}
+	}
+
+	for tag := table.FindNextSibling(); tag.NodeValue != "div"; tag = tag.FindNextSibling() {
+		parsed, err := parseTag(tag)
+		if err != nil {
+			return teacherProfile{}, err
+		}
+		result.desc += parsed
+	}
+
+	return result, nil
+}
